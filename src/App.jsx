8b6c400769f9554c7c49b6d5a99e8199230863d8
John <cooljohne@gmail.com>
2026-02-16 00:11:20 +0100

    Form Format

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,33 @@ import Login from './pages/login'
 import Signup from './pages/signup'
 import Homepage from './pages/homepage'
 import Dashboard from './pages/dashboard'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([
+  {
+    index: true,
+    element: <Homepage />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  }
+])
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      index: true,
-      element: <Homepage />
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/signup',
-      element: <Signup />
-    },
-    {
-      path: '/dashboard',
-      element: <Dashboard />
-    }
-  ])
-
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="w-full max-w-4xl p-6 bg-white shadow-md rounded-lg">
+        <RouterProvider router={router} />
+      </div>
     </div>
   )
 }
